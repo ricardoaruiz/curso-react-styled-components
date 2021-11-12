7d4c8c98c205ff57c6191bbde6abce1de6d1ce28
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-11-12 09:04:20 -0300

    feat: using media query

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -8,6 +8,12 @@ const Container = styled.div`
   background-color: #f1f1f1;
   min-height: 90vh;
   padding: 0px 15vw;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+}
+
 `
 
 const Content = styled.section`
